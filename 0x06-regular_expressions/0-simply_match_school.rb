f4b4c6ac4d8dcfa4f8c52b2403b997eaa40080a1
aouraghse <aouraghse@gmail.com>
2023-10-31 05:08:07 -0700

    task 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,18 +1,2 @@
 #!/usr/bin/env ruby
-# Check if the argument contains the word "School"
-def match_school(input)
-  pattern = /School/
-  if input.match?(pattern)
-    puts "The string '#{input}' contains the word 'School'."
-  else
-    puts "The string '#{input}' does not contain the word 'School'."
-  end
-end
-
-# Check if an argument was provided
-if ARGV.empty?
-  puts "Please provide a string as an argument."
-else
-  input = ARGV[0]
-  match_school(input)
-end
+puts ARGV[0].scan(/School/).join
